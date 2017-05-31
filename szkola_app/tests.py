44277817c4a7635856d3b9b6d24063c4832c0bb0
Piotr Kolodziejczyk <piter.kolod@gmail.com>
2017-06-01 00:09:26 +0200

    dodane testy 2 modeli

--- szkola_app/tests.py
+++ szkola_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from szkola_app.models import *
 
 # Create your tests here.
+
+
+class SchoolSubjectTestCase(TestCase):
+    def test_string_representation(self):
+        math = SchoolSubject.objects.create(name="matematyka")
+        self.assertEqual(str(math), math.name)
+
+
+class TeacherTestCase(TestCase):
+    def test_string_representation(self):
+        math = SchoolSubject.objects.create(name="matematyka")
+        teacher = Teacher.objects.create(first_name="Jan", last_name="Janowski", school_subject=math)
+        self.assertEqual(str(teacher), "Jan Janowski matematyka")
